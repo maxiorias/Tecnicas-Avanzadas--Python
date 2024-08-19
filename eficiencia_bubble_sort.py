23b99318777a8edfa3ca7f2f83bdb3d1c8a73a25
Maxi <maxiorias@gmail.com>
2024-08-19 16:30:45 -0300

    eficiencia bubble sort

---
+++ eficiencia_bubble_sort.py
@@ -0,0 +1,19 @@
+import time
+import random
+
+# Genera datos de prueba
+arr = [random.randint(1, 1000000) for _ in range(5000)]
+
+def ordenamiento_burbuja(arr):
+    n = len(arr)
+    for i in range(n - 1):  
+        for j in range(n - 1):  # Optimización del bucle interno
+            if arr[j] > arr[j + 1]: 
+                aux = arr[j] 
+                arr[j] = arr[j + 1]
+                arr[j + 1] = aux
+
+# Medición de tiempo para ordenamiento burbuja
+start_time = time.time()
+ordenamiento_burbuja(arr.copy())
+print("Tiempo de ejecución burbuja: %s segundos" % (time.time() - start_time))
