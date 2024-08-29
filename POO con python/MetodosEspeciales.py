aeafaaf32f6ca5e8b77fb8111bf97fb220842c82
Maxi <maxiorias@gmail.com>
2024-08-29 13:24:36 -0300

    add: MetodosEspeciales

--- POO con python/MetodosEspeciales.py
+++ POO con python/MetodosEspeciales.py
@@ -3,16 +3,30 @@ class auto:
         self.marca = marca
         self.kilometros = kilometros
         self.anio = anio
+        #self.__ruedas = 4
         print(f'se ha creado un auto marca {self.marca}')
         print()
     
-    def __del__(self):
-        print(f'se ha vendido un auto marca {self.marca}')
-        print()
+    #def __del__(self):
+    #    print(f'se ha vendido un auto marca {self.marca}')
+    #   print()
+
+    def arrancar(self, arrancamos):
+        self.enmarcha = arrancamos
+        if(self.enmarcha):
+            return 'El auto esta encendido'
+        else:
+            return 'El auto esta apagado'
     
     def __str__(self):
-        return 'el auto es un {}, tiene {}, y es del año {}'.format(self.marca, self.kilometros, self.anio)
+        return 'el auto es un {}, tiene {} mil kilometros, y es del año {}'.format(self.marca, self.kilometros, self.anio)
 
+print()
 auto_uno = auto('audi', 200.000, 1993)
-
-print(str(auto_uno))
+auto_dos = auto('fiat', 500, 2005)
+##print(auto_uno.__ruedas)
+##print(str(auto_uno))
+print(str(auto_dos))
+print(auto_uno.arrancar(True))
+print(auto_dos.arrancar(False))
+print()
